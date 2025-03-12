abf2292bae0f0f4d7c37db68225579f4b585cdd4
Vu123-jpg <aonhay123ty@gmail.com>
2025-03-12 15:08:38 +0700

    Add hello.cpp

---
+++ hello.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+
+std::string getGameOverMessage() {
+    return "Game Over!";
+}
+
+int main() {
+    std::cout << getGameOverMessage() << std::endl;
+    return 0;
+}
